2ff1eb84e50d1e6f57a6ed88712b30654301c2f3
MC-Games\MC <martin.crabtree@gmail.com>
2020-03-04 22:32:37 -0500

    fleshed out Interface example with updated syntax

--- src/components/DemoInterface.tsx
+++ src/components/DemoInterface.tsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 // using interface to pass objects to useState while using TypeScript
-interface UserInfo{
+interface IUserInfo{
     firstname: string;
     lastname: string;
 }
 
 const DemoInterface: React.FC=()=>{
+    const firstUser = { 
+        firstname: "Stewart",
+        lastname: "little"  
+    };
+
+    const [ userInfo, setUserInfo ] = useState<IUserInfo | null>();
+
 
     return(
         <div>
-
+            <div>First Name: { userInfo?.firstname } </div>
+            <div>Last Name: { userInfo?.lastname } </div>
+            <button onClick={ ()=>{!userInfo ? setUserInfo(firstUser) : setUserInfo(null)} } > Change Name </button>
         </div>
     );
 
 }
 
 export default DemoInterface;
+
